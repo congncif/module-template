0001a4ada29a11fc72680a74300261eea96eea15
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-02-25 10:59:42 +0700

    Update component lifecycle

--- Templates/ComponentB.xctemplate/___FILEBASENAME___Board.swift
+++ Templates/ComponentB.xctemplate/___FILEBASENAME___Board.swift
@@ -14,14 +14,19 @@ final class ___VARIABLE_moduleName___Board: Board, GuaranteedBoard {
 
     private let builder: ___VARIABLE_moduleName___Buildable
 
-    private lazy var component = builder.build(withDelegate: self)
-
     init(identifier: BoardID = <#.___VARIABLE_moduleName___#>, builder: ___VARIABLE_moduleName___Buildable) {
         self.builder = builder
         super.init(identifier: identifier)
     }
 
     func activate(withGuaranteedInput input: InputType) {
+        if let controller = rootViewController.firstAttachedObject(___VARIABLE_moduleName___Controllable.self) {
+            controller.activate()
+            return
+        }
+        
+        let component = builder.build(withDelegate: self)
+        rootViewController.attachObject(component.controller)
         component.controller.activate()
     }
 }
--- Templates/VaIP-IDM.xctemplate/___FILEBASENAME___Board.swift
+++ Templates/VaIP-IDM.xctemplate/___FILEBASENAME___Board.swift
@@ -15,15 +15,20 @@ final class ___VARIABLE_moduleName___Board: Board, GuaranteedBoard {
 
     private let builder: ___VARIABLE_moduleName___Buildable
 
-    private lazy var component = builder.build(withDelegate: self)
-
     init(identifier: BoardID = <#.___VARIABLE_moduleName___#>, builder: ___VARIABLE_moduleName___Buildable) {
         self.builder = builder
         super.init(identifier: identifier)
     }
 
     func activate(withGuaranteedInput input: InputType) {
+        if let controller = rootViewController.firstAttachedObject(___VARIABLE_moduleName___Controllable.self) {
+            controller.activate()
+            return
+        }
+        
+        let component = builder.build(withDelegate: self)
         component.adapter.connect(with: rootViewController)
+        rootViewController.attachObject(component.controller)
         component.controller.activate()
     }
 }
