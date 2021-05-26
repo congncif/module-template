0a31ce7ed1be9dedd548f9fc93d1f558ea122d2a
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-05-26 10:00:20 +0700

    [Board Template] ContinuousBoard by default

--- Templates/Boardy.xctemplate/___VARIABLE_moduleName___Board.swift
+++ Templates/Boardy.xctemplate/___VARIABLE_moduleName___Board.swift
@@ -10,14 +10,16 @@ import Boardy
 import Foundation
 import UIKit
 
-final class ___VARIABLE_moduleName___Board: Board, GuaranteedBoard {
+final class ___VARIABLE_moduleName___Board: ContinuousBoard, GuaranteedBoard, GuaranteedOutputSendingBoard {
 	typealias InputType = <#Any#>
+    typealias OutputType = <#Any#>
 
     private let builder: ___VARIABLE_moduleName___Buildable
 
-    init(identifier: BoardID, builder: ___VARIABLE_moduleName___Buildable) {
+    init(identifier: BoardID, builder: ___VARIABLE_moduleName___Buildable, continuousBoard: FlowMotherboard) {
         self.builder = builder
-        super.init(identifier: identifier)
+        super.init(identifier: identifier, motherboard: continuousBoard)
+        registerFlows()
     }
 
     func activate(withGuaranteedInput input: InputType) {
@@ -27,3 +29,7 @@ final class ___VARIABLE_moduleName___Board: Board, GuaranteedBoard {
 }
 
 extension ___VARIABLE_moduleName___Board: ___VARIABLE_moduleName___Delegate {}
+
+private extension ___VARIABLE_moduleName___Board {
+    func registerFlows() {}
+}
