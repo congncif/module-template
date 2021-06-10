abd89f32be6a758bf0e42dd7b45a479dda011114
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-06-10 10:40:30 +0700

    Update IO Interface follow Boardy 1.18.1

--- Templates/Boardy.xctemplate/___FILEBASENAME___IOInterface.swift
+++ Templates/Boardy.xctemplate/___FILEBASENAME___IOInterface.swift
@@ -9,21 +9,21 @@
 import Boardy
 import Foundation
 
-public typealias ___VARIABLE_moduleName___Input = Any?
+typealias ___VARIABLE_moduleName___Input = Any?
 
-public enum ___VARIABLE_moduleName___Output {}
+enum ___VARIABLE_moduleName___Output {}
 
-public enum ___VARIABLE_moduleName___Command {}
+enum ___VARIABLE_moduleName___Command {}
 
 // MARK: - Quick Access
 
-public struct ___VARIABLE_moduleName___Destination {
-    public let activation: BoardActivation<___VARIABLE_moduleName___Input>
-    public let interaction: BoardInteraction<___VARIABLE_moduleName___Command>
+struct ___VARIABLE_moduleName___Destination {
+    let activation: BoardActivation<___VARIABLE_moduleName___Input>
+    let interaction: BoardInteraction<___VARIABLE_moduleName___Command>
 }
 
 extension ActivatableBoard {
-    public func io___VARIABLE_moduleName___(_ identifier: BoardID) -> ___VARIABLE_moduleName___Destination {
+    func io___VARIABLE_moduleName___(_ identifier: BoardID) -> ___VARIABLE_moduleName___Destination {
         ___VARIABLE_moduleName___Destination(
             activation: activation(identifier, with: ___VARIABLE_moduleName___Input.self),
             interaction: interaction(identifier, with: ___VARIABLE_moduleName___Command.self)
@@ -32,7 +32,7 @@ extension ActivatableBoard {
 }
 
 extension MotherboardType {
-    public func io___VARIABLE_moduleName___(_ identifier: BoardID) -> ___VARIABLE_moduleName___Destination {
+    func io___VARIABLE_moduleName___(_ identifier: BoardID) -> ___VARIABLE_moduleName___Destination {
         ___VARIABLE_moduleName___Destination(
             activation: activation(identifier, with: ___VARIABLE_moduleName___Input.self),
             interaction: interaction(identifier, with: ___VARIABLE_moduleName___Command.self)
@@ -41,7 +41,7 @@ extension MotherboardType {
 }
 
 extension FlowManageable {
-    public func io___VARIABLE_moduleName___Flow(_ identifier: BoardID) -> FlowHandler<___VARIABLE_moduleName___Output> {
+    func io___VARIABLE_moduleName___Flow(_ identifier: BoardID) -> FlowHandler<___VARIABLE_moduleName___Output> {
         matchedFlow(identifier, with: ___VARIABLE_moduleName___Output.self)
     }
 }
