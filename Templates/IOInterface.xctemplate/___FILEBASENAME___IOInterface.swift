cfb5ca1840b36b9c5faf10c0e46261f800b02650
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-09-01 07:23:20 +0700

    Avoid expose multiple namespaces of one module

--- Templates/IOInterface.xctemplate/___FILEBASENAME___IOInterface.swift
+++ Templates/IOInterface.xctemplate/___FILEBASENAME___IOInterface.swift
@@ -11,8 +11,8 @@ import Foundation
 
 // MARK: - Namespace
 
-public enum ___VARIABLE_moduleName___Namespace {
-    public static let `default`: BoardID = "pub.mod.___TARGETNAME___.___VARIABLE_moduleName___"
+extension BoardID {
+    static let pub___VARIABLE_moduleName___: BoardID = "pub.mod.___VARIABLE_moduleName___"   
 }
 
 // MARK: - Interface
@@ -20,10 +20,12 @@ public enum ___VARIABLE_moduleName___Namespace {
 public struct ___VARIABLE_moduleName___Destination {
     public let activation: BoardActivation<___VARIABLE_moduleName___Parameter>
     public let interaction: BoardInteraction<___VARIABLE_moduleName___Command>
+
+    public static let defaultIdentifier: BoardID = .pub___VARIABLE_moduleName___
 }
 
 extension ActivatableBoard {
-    public func io___VARIABLE_moduleName___(_ identifier: BoardID = ___VARIABLE_moduleName___Namespace.default) -> ___VARIABLE_moduleName___Destination {
+    public func io___VARIABLE_moduleName___(_ identifier: BoardID = ___VARIABLE_moduleName___Destination.defaultIdentifier) -> ___VARIABLE_moduleName___Destination {
         ___VARIABLE_moduleName___Destination(
             activation: activation(identifier, with: ___VARIABLE_moduleName___Parameter.self),
             interaction: interaction(identifier, with: ___VARIABLE_moduleName___Command.self)
@@ -35,10 +37,12 @@ public struct ___VARIABLE_moduleName___MainDestination {
     public let activation: MainboardActivation<___VARIABLE_moduleName___Parameter>
     public let interaction: MainboardInteraction<___VARIABLE_moduleName___Command>
     public let flow: FlowHandler<___VARIABLE_moduleName___Output>
+
+    public static let defaultIdentifier: BoardID = .pub___VARIABLE_moduleName___
 }
 
 extension MotherboardType where Self: FlowManageable {
-    public func io___VARIABLE_moduleName___(_ identifier: BoardID = ___VARIABLE_moduleName___Namespace.default) -> ___VARIABLE_moduleName___MainDestination {
+    public func io___VARIABLE_moduleName___(_ identifier: BoardID = ___VARIABLE_moduleName___MainDestination.defaultIdentifier) -> ___VARIABLE_moduleName___MainDestination {
         ___VARIABLE_moduleName___MainDestination(
             activation: activation(identifier, with: ___VARIABLE_moduleName___Parameter.self),
             interaction: interaction(identifier, with: ___VARIABLE_moduleName___Command.self),
