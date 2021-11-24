3ea6d834957c70a635fbcbc8b4c6b9a59164e288
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-11-24 14:20:57 +0700

    Set clear mod ID

--- Templates/IO Interface.xctemplate/Public Interface only/___FILEBASENAME___IOInterface.swift
+++ Templates/IO Interface.xctemplate/Public Interface only/___FILEBASENAME___IOInterface.swift
@@ -12,7 +12,7 @@ import Foundation
 // MARK: - ID
 
 extension BoardID {
-    static let pub___VARIABLE_moduleName___: BoardID = "pub.mod.___VARIABLE_moduleName___"   
+    static let pub___VARIABLE_moduleName___: BoardID = "pub.mod.___TARGETNAME___.___VARIABLE_moduleName___"   
 }
 
 // MARK: - Interface
