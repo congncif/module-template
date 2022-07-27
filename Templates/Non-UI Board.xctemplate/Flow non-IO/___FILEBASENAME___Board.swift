9db8ee12362e8a5c9bf10d7b73a77e18cfe55b5a
NGUYEN CHI CONG <congnc.if@gmail.com>
2022-07-27 16:56:23 +0700

    Add flow non-IO

---
+++ Templates/Non-UI Board.xctemplate/Flow non-IO/___FILEBASENAME___Board.swift
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___TARGETNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Compatible with Boardy 1.47 or later
+//
+
+import Boardy
+import Foundation
+
+enum ___VARIABLE_moduleName___BoardFactory {
+    static func make(identifier: BoardID, producer: ActivatableBoardProducer) -> ActivatableBoard {
+        FlowBoard<___VARIABLE_moduleName___Input, ___VARIABLE_moduleName___Output, ___VARIABLE_moduleName___Command, ___VARIABLE_moduleName___Action>(identifier: identifier, producer: producer) { it in
+            <#register flows#>
+        } flowActivation: { it, input in
+            <#activate#>
+        }
+    }
+}
