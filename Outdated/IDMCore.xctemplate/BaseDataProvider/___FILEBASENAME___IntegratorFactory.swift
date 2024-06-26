aee3163f8030282f41426501ca2b9f783a02c28e
NGUYEN CHI CONG <congnc.if@gmail.com>
2024-06-26 15:50:52 +0700

    //  Compatible with Boardy 1.54 or later

--- Outdated/IDMCore.xctemplate/BaseDataProvider/___FILEBASENAME___IntegratorFactory.swift
+++ Outdated/IDMCore.xctemplate/BaseDataProvider/___FILEBASENAME___IntegratorFactory.swift
@@ -3,7 +3,7 @@
 //  ___TARGETNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Compatible with Boardy 1.47 or later
+//  Compatible with Boardy 1.54 or later
 //
 
 import Foundation
