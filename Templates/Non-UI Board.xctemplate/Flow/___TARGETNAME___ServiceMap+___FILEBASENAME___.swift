fd170218592b171af2d4a4dca47c454681007bb9
NGUYEN CHI CONG <congnc.if@gmail.com>
2025-09-13 12:17:10 +0700

    Add service map default

---
+++ Templates/Non-UI Board.xctemplate/Flow/___TARGETNAME___ServiceMap+___FILEBASENAME___.swift
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___TARGETNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Compatible with Boardy 1.55.1 or later
+//
+
+import Boardy
+import Foundation
+
+extension ___TARGETNAME___ServiceMap {
+    var io___VARIABLE_moduleName___: ___VARIABLE_moduleName___MainDestination {
+        mainboard.io___VARIABLE_moduleName___()
+    }
+}
---
+++ Templates/Service Map.xctemplate/Internal Plugins module/___TARGETNAME___ServiceMap.swift
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___TARGETNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Compatible with Boardy 1.55.1 or later
+//
+
+import Boardy
+import Foundation
+
+final class ___TARGETNAME___ServiceMap: ServiceMap {}
+
+extension ServiceMap {
+    var mod___TARGETNAME___: ___TARGETNAME___ServiceMap { link() }
+}
